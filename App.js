26ff72fe68ebad77cf4d5cc9f1e3b28392050e9a
Muhammad Usman <muhammad.usman.1697@gmail.com>
2019-12-01 23:11:39 +0500

    Added Login Screen

--- App.js
+++ App.js
@@ -2,14 +2,15 @@ import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import Signup from './src/screens/SignupScreen';
 import Login from './src/screens/LoginScreen';
 import SplashScreen from './src/screens/SplashScreen';
-
+import DashboardScreen from './src/screens/DashboardScreen';
 
 const navigator = createSwitchNavigator({
   Splash: SplashScreen,
   Login: Login,
   Signup: Signup,
+  Dashboard: DashboardScreen,
 }, {
-  initialRouteName: 'Signup',
+  initialRouteName: 'Login',
 });
 
 const App = createAppContainer(navigator);
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,17 +2,16 @@ import React, { Component } from 'react';
 import {
     View,
     StyleSheet,
-    Alert,
-    Image,
-    KeyboardAvoidingView,
+    TouchableOpacity,
     Dimensions,
+    Text,
 } from 'react-native';
 import {
     Text as EText,
     Button as EButton,
     Input as EInput,
 } from 'react-native-elements';
-import FAIcon from 'react-native-vector-icons/FontAwesome';
+import AntIcon from 'react-native-vector-icons/AntDesign';
 import { withNavigation } from 'react-navigation';
 import authApi from '../api/authApi';
 
@@ -27,105 +26,147 @@ class LoginForm extends Component {
             email: '',
             password: '',
             emailErrMsg: '',
-            passwordErrMsg: '',
+            passwordErrMsg: '   ',
         };
+        this._clearEmailErrMsg = this._clearEmailErrMsg.bind(this);
+        this._clearPasswordErrMsg = this._clearPasswordErrMsg.bind(this);
+        this._handleEmailInput = this._handleEmailInput.bind(this);
+        this._handlePasswordInput = this._handlePasswordInput.bind(this);
+        this._handleLogin = this._handleLogin.bind(this);
+        this._checkEmail = this._checkEmail.bind(this);
+        this._checkPassword = this._checkPassword.bind(this);
+    }
+
+    _clearEmailErrMsg() {
+        this.setState({ emailErrMsg: '' });
+    }
+
+    _clearPasswordErrMsg() {
+        this.setState({ passwordErrMsg: '' });
+    }
+
+    _handleEmailInput(email) {
+        this.setState({email});
+    }
+
+    _handlePasswordInput(password) {
+        this.setState({password});
+    }
+
+    _checkEmail() {
+        if(this.state.email.length != 0) {
+            let re_email = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+[^<>()\.,;:\s@\"]{2,})$/;
+            if (!re_email.test(this.state.email)) {
+                this.setState({ emailErrMsg: 'Invalid Email' });
+            }
+        }
+    }
+
+    _checkPassword() {
+        if(this.state.password.length != 0) {
+            let re_password = /\S{6,}/;
+            if (!re_password.test(this.state.password)) {
+                this.setState({ passwordErrMsg: 'Password length must be 6 characters' });
+            }
+        }
+    }
+
+    _handleLogin() {
+        let re_email = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+[^<>()\.,;:\s@\"]{2,})$/;
+        let re_password = /\S{6,}/;
+        if(!re_email.test(this.state.email)) {
+            this.setState({emailErrMsg: 'Invalid Email'});
+        }
+        if(!re_password.test(this.state.password)) {
+            this.setState({passwordErrMsg: 'Password length must be 6 characters'});
+        }
+        if(re_email.test(this.state.email) && re_password.test(this.state.password)) {
+            this.setState({loading: true});
+            authApi.post('', {
+                email: this.state.email,
+                password: this.state.password,
+                returnSecureToken: true,
+            }).then( res => {
+                if(res.data.registered) {
+                    this.props.navigation.navigate('Dashboard');
+                }
+            }).catch(err => {
+                this.setState({ loading: false });
+                if(err.response.data.error) {
+                    alert("Incorrect Username or Password");
+                }
+            });
+        }
     }
 
     render() {
         return (
             <>
-                <KeyboardAvoidingView behavior="position" style={styles.container} >
-                    <Image
-                        source={require(`../../assets/logo.jpg`)}
-                        style={styles.logo}
-                        resizeMethod="scale"
-                    />
+                <View style={styles.formContainer} >
                     <EInput
-                        label="Email"
-                        value={this.state.email}
-                        errorStyle={styles.inputErrorStyle}
-                        onChangeText={(email) => this.setState({ email })}
-                        leftIcon={<FAIcon name="user" size={35} color="white" />}
-                        placeholder="Enter Email"
-                        inputStyle={{ paddingLeft: 10, fontSize: 20 }}
+                        leftIcon={<AntIcon name="user" size={20} color="white" />}
+                        containerStyle={StyleSheet.flatten(styles.inputWraper, { marginTop: 15 })}
+                        inputContainerStyle={{ borderBottomWidth: 1, borderBottomColor: 'rgba(0,0,0,0)', 
+                        flex: 1, marginBottom: '2%' }}
+                        inputStyle={styles.inputStyle}
+                        placeholder="Username"
+                        placeholderTextColor="white"
+                        onEndEditing={this._checkEmail}
+                        onChangeText={this._handleEmailInput}
                         errorMessage={this.state.emailErrMsg}
-                        inputContainerStyle={styles.inputConainerStyle}
-                        placeholderTextColor="white"
-                        labelStyle={styles.inputLabelStyle}
-                        containerStyle={styles.containerStyle}
+                        onFocus={this._clearEmailErrMsg}
+                        errorStyle={styles.errorMsgStyle}
                     />
+                    <View style={styles.hr} />
                     <EInput
-                        label="Password"
-                        placeholder="Enter Password"
-                        errorStyle={styles.inputErrorStyle}
-                        onChangeText={(password) => this.setState({ password })}
+                        leftIcon={<AntIcon name="lock" size={20} color="white" />}
+                        containerStyle={styles.inputWraper}
+                        inputContainerStyle={styles.inputUsername}
+                        inputStyle={styles.inputStyle}
+                        placeholder="Password"
+                        onEndEditing={this._checkPassword}
+                        onChangeText={this._handlePasswordInput}
                         secureTextEntry
-                        leftIcon={<FAIcon name="lock" size={37} color="white" />}
-                        errorMessage={this.state.passwordErrMsg}
-                        inputStyle={{ paddingLeft: 12, fontSize: 20 }}
-                        inputContainerStyle={styles.inputConainerStyle}
                         placeholderTextColor="white"
-                        labelStyle={styles.inputLabelStyle}
-                        containerStyle={styles.containerStyle}
+                        errorMessage={this.state.passwordErrMsg}
+                        onFocus={this._clearPasswordErrMsg}
+                        errorStyle={styles.errorMsgStyle}
                     />
-                    <EButton
-                        title="Login"
-                        loading={false}
-                        containerStyle={styles.btnContainerStyle}
-                        buttonStyle={styles.btnStyle}
-                        titleStyle={styles.btnTitleStyle}
-                        icon={<FAIcon name="sign-in" size={30} color="white" style={{ marginLeft: 15, }} />}
-                        iconContainerStyle={styles.btnIconContainerStyle}
-                        loading={this.state.loading}
-                        loadingProps={{ size: "large" }}
-                        iconRight={true}
-                        onPress={(event) => {
-                            let re_email = /\S+@\S+\.\S+/;
-                            let re_password = /\S{6,}/;
-                            if (re_email.test(this.state.email) == true) {
-                                this.setState({ emailErrMsg: '' });
-                            } else {
-                                this.setState({emailErrMsg: 'Invalid Email'});
-                            }
-                            if (re_password.test(this.state.password) == true) {
-                                this.setState({ passwordErrMsg: '' });
-                            } else {
-                                this.setState({ passwordErrMsg: 'Password must be 4 characters' });
-                            }
-                            if (re_email.test(this.state.email) && re_password.test(this.state.password)) {
-                                this.setState({ loading: true });
-                                authApi.post('', {
-                                    email: this.state.email,
-                                    password: this.state.password,
-                                    returnSecureToken: true
-                                }).then((res) => {
-                                    console.log(res.data);
-                                    if(res.data.registered) {
-                                        this.props.navigation.navigate('Dashboard');
-                                    }
-                                }).catch((err) => {
-                                    Alert.alert(
-                                        'Login Failed',
-                                        'Incorrect Email or Password',
-                                        [
-                                            { text: 'OK', onPress: () => console.log('OK Pressed') },
-                                        ],
-                                        { cancelable: true, onDismiss: () => {}},
-                                    );
-                                    this.setState({loading: false});
-                                });
-                            }
-                        }}
-                    />
-                    <EButton
-                        title="Signup"
-                        buttonStyle={styles.btnStyle}
-                        containerStyle={styles.btnContainerStyle}
-                        onPress={() => {
-                            this.props.navigation.navigate('Signup');
-                        }}  
-                    />
-                </KeyboardAvoidingView>
+                </View>
+                <TouchableOpacity>
+                    <Text style={styles.forgotPassword} >
+                        Forgot Password?
+                    </Text>
+                </TouchableOpacity>
+                <EButton
+                    containerStyle={styles.loginBtnWrapperStyle}
+                    buttonStyle={styles.loginBtnSyle}
+                    titleStyle={styles.loginBtnTitleStyle}
+                    onPress={this._handleLogin}
+                    loading={this.state.loading}
+                    loadingStyle={styles.loadingStyle}
+                    loadingProps={{ color: '#3185E8', size: 'large'}}
+                    title="Login"
+                />
+                <View style={{ flexDirection: 'row' }} >
+                    <Text style={{
+                        marginLeft: '15%',
+                        fontSize: 16,
+                        alignSelf: 'center'
+                    }} >
+                        Don't Have an Account? 
+                    </Text>
+                    <TouchableOpacity style={{
+                        marginLeft: '2%',
+                    }} >
+                        <Text style={{
+                            fontSize: 16,
+                            color: '#3185E8',
+                        }} >
+                            Sign Up Now!!!
+                        </Text>
+                    </TouchableOpacity>
+                </View>
             </>
         );
     }
@@ -135,49 +176,64 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
-    bg: {
-        resizeMode: 'cover',
-        width: '100%',
-        height: '100%',
-    },
-    logo: {
-        opacity: 0.9,
-        height: 230,
-        width: 230,
-        borderRadius: 100,
-        alignSelf: 'center',
-        resizeMode: 'contain',
-        marginTop: "10%",
-    },
-    containerStyle: {
+    formContainer: {
+        height: 180,
         width: WIDTH - 50,
-        marginLeft: 15,
-    },
-    inputConainerStyle: {
-        borderBottomWidth: 0,
-        paddingLeft: 0,
-    },
-    inputLabelStyle: {
-        color: 'white',
-        fontSize: 25,
-        marginRight: 15,
-    },
-    inputErrorStyle: {
-        fontSize: 18,
-    },
-    btnContainerStyle: {
-        marginTop: 20,
-        width: WIDTH - 150,
-        alignSelf: 'center',
-        borderRadius: 20,
-    },
-    btnStyle: {
         borderRadius: 25,
+        alignSelf: 'center',
+        marginTop: '15%',
+        backgroundColor: '#3185E8',
+        flexDirection: 'column',
+        justifyContent: 'center',
     },
-    btnTitleStyle: {
-        fontSize: 25,
+    hr: {
+        borderBottomColor: '#fff',
+        marginHorizontal: 25,
+        borderBottomWidth: 1,
     },
-    btnIconContainerStyle: {
+    inputWraper: {
+        flex: 1,
+        marginLeft: '2%',
+        alignSelf: 'stretch',
+    },
+    inputStyle: {
+        borderBottomWidth: 0,
+        fontSize: 23,
+    },
+    inputUsername: { 
+        borderBottomWidth: 1, 
+        borderBottomColor: 'rgba(0,0,0,0)', 
+    },
+    errorMsgStyle: {
+        fontSize: 18,
+        marginLeft: '2%',
+        marginBottom: '2%',
+    },
+    forgotPassword: {
+        fontSize: 18,
+        color: '#3185E8',
+        alignSelf: 'flex-end',
+        marginTop: '2%',
+        marginRight: '8%',
+    },
+    loginBtnWrapperStyle: {
+        width: WIDTH - 200, 
+        alignSelf: 'center', 
+        borderColor: "#3185E8", 
+        borderWidth: 1,
+        marginVertical: '5%',
+    },
+    loginBtnSyle: { 
+        borderRadius: 25, 
+        backgroundColor: "#eeeeee" 
+    },
+    loginBtnTitleStyle: { 
+        color: '#3185E8',
+        fontSize: 22, 
+    },
+    loadingStyle: {
+        alignSelf: 'center',
+        height: 20,
     },
 });
 
